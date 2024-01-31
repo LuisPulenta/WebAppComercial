2bec814a779b4f0234333b28dcea1ff86a4b5333
LuisPulenta <luisalbertonu@gmail.com>
2024-01-31 18:04:50 -0300

    CategoriesController

---
+++ WebAppComercial.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppComercial.Api.Data;
+using WebAppComercial.Shared.Entities;
+
+namespace WebAppComercial.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpPost]
+        public async Task<ActionResult> Post(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+    }
+}
---
+++ WebAppComercial.Api/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppComercial.Shared.Entities;
+
+namespace WebAppComercial.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
