3309011fa2e84ae96e21e4c0fd2635281b2fbafe
LuisPulenta <luisalbertonu@gmail.com>
2024-03-03 11:52:52 -0300

    Compras

---
+++ WebAppComercial.Api/Controllers/BuyDetailsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAppComercial.Api.Data;
+using WebAppComercial.Shared.DTOs;
+using WebAppComercial.Shared.Entities;
+
+namespace WebAppComercial.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/buydetails")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class BuyDetailsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BuyDetailsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(BuyDetailDTO buydetailDTO)
+        {
+            if (buydetailDTO is null)
+            {
+                throw new ArgumentNullException(nameof(buydetailDTO));
+            }
+
+            Buy? buy = await _context.Buys!.FindAsync(buydetailDTO.BuyId);
+            Product? product = await _context.Products!.FindAsync(buydetailDTO.ProductId);
+
+            BuyDetail newBuyDetail = new BuyDetail();
+            newBuyDetail.BuyId = buydetailDTO.BuyId;
+            newBuyDetail.ProductId = buydetailDTO.ProductId;
+            newBuyDetail.Buy = buy!;
+            newBuyDetail.Product = product!;
+            newBuyDetail.Cost = buydetailDTO.Cost;
+            newBuyDetail.Name = buydetailDTO.Name;
+            newBuyDetail.Quantity = buydetailDTO.Quantity;
+            newBuyDetail.MoveId = buydetailDTO.MoveId;
+            newBuyDetail.PercentageDiscount = buydetailDTO.PercentageDiscount;
+            newBuyDetail.PercentageIVA = buydetailDTO.PercentageIVA;
+
+            _context.Add(newBuyDetail);
+            
+            await _context.SaveChangesAsync();
+            return Ok(newBuyDetail);            
+        }
+    }
+}
--- WebAppComercial.Shared/DTOs/BuyDetailDTO.cs
+++ WebAppComercial.Shared/DTOs/BuyDetailDTO.cs
@@ -1,28 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebAppComercial.Shared.Entities
+namespace WebAppComercial.Shared.DTOs
 {
-    public class BuyDetail
+    public class BuyDetailDTO
     {
         public int Id { get; set; }
-
-
-        [Display(Name = "N° Compra")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public Buy Buy { get; set; } = null!;
 
         [Display(Name = "N° Compra")]
         public int BuyId { get; set; }
 
-
-        [Display(Name = "Producto")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public Product Product { get; set; } = null!;
         [Display(Name = "Producto")]
         public int ProductId { get; set; }
-
-        
 
         [Display(Name = "Nombre")]
         [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
@@ -40,14 +29,8 @@ namespace WebAppComercial.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public double Quantity { get; set; }
 
-        //[Display(Name = "Kardex")]
-        //[Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        //public Move Move { get; set; } = null!;
-
         [Display(Name = "Kardex")]
         public int MoveId { get; set; }
-
-
 
         [DisplayFormat(DataFormatString = "{0:P2}")]
         [Display(Name = "%IVA")]
