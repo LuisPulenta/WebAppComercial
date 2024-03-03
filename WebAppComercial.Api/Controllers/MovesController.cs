0efa1f829254f256320cd32762c42c7178ee31f8
LuisPulenta <luisalbertonu@gmail.com>
2024-03-03 11:23:39 -0300

    Compras

---
+++ WebAppComercial.Api/Controllers/MovesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppComercial.Api.Data;
+using WebAppComercial.Shared.DTOs;
+using WebAppComercial.Shared.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAppComercial.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/moves")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class MovesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MovesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpGet("lastmove/{storeid:int}/{productid:int}")]
+        public async Task<IActionResult> GetLastMoveAsync(int storeid, int productid)
+        {
+            List<Move> moves = await _context.Moves
+                .Where(x => x.StoreId == storeid && x.ProductId == productid)
+                .ToListAsync();
+
+            Move lastmove = moves.OrderByDescending(x => x.Date).FirstOrDefault()!;
+
+            if (lastmove ==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(lastmove);
+            }            
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpGet("getmoves/{storeid:int}/{productid:int}")]
+        public async Task<IActionResult> GetAsync(int storeid, int productid)
+        {
+            List<Move> moves = await _context.Moves
+                .Where(x => x.StoreId == storeid && x.ProductId == productid)
+                .OrderBy(x=>x.Date)
+                .ToListAsync();
+
+            return Ok(moves);
+        }
+
+        //---------------------------------------------------------------------------------------
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(MoveDTO moveDTO)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Product? product = await _context.Products!.FindAsync(moveDTO.ProductId);
+            Store? store = await _context.Stores!.FindAsync(moveDTO.StoreId);
+
+            Move newMove = new()
+            {
+                Store = store!,
+                StoreId = moveDTO.StoreId,
+                Product = product!,
+                ProductId = moveDTO.ProductId,
+                AverageCost = moveDTO.AverageCost,
+                Balance = moveDTO.Balance,
+                Date = moveDTO.Date,
+                Document = moveDTO.Document,
+                Entrance = moveDTO.Entrance,
+                Exit = moveDTO.Exit,
+                LastCost = moveDTO.LastCost,
+            };
+            _context.Add(newMove);
+            await _context.SaveChangesAsync();
+            return Ok(newMove);
+                 }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+      }
+}
---
+++ WebAppComercial.Shared/DTOs/MoveDTO.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppComercial.Shared.DTOs
+{
+    public class MoveDTO
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Almacén")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int StoreId { get; set; }
+
+
+        [Display(Name = "Producto")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Fecha")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Documento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; } = null!;
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Entrada")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public double Entrance { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Salida")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public double Exit { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Saldo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public double Balance { get; set; }
+
+        [Column(TypeName = "decimal(18,3)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Ultimo Costo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal LastCost { get; set; }
+
+        [Column(TypeName = "decimal(18,3)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Costo Promedio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal AverageCost { get; set; }
+    }
+}
